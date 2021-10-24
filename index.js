a2f68f8ea99922a0b94d45865d77dd03f5596cec
422404 <elyanpoujol@gmail.com>
2021-10-24 17:04:43 +0200

    Added build config type checking

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 
 import Handlebars from 'handlebars';
+import { checkNamedParams } from 'scpwiki-handlebars-util';
 
 /**
  * @typedef {Object} BuildOptions
@@ -16,6 +17,14 @@ const handlebarsOptions = {
     strict: true
 };
 
+const buildOptionsSpec = {
+    entry: 'string',
+    partialsDir: 'string',
+    output: 'object',
+    data: 'object',
+    components: 'object'
+};
+
 /**
  * Builds a template file.
  * @param {BuildOptions} options
@@ -23,16 +32,14 @@ const handlebarsOptions = {
 export const build = async (options) => {
     const h = Handlebars.create();
 
+    checkNamedParams(buildOptionsSpec, options);
+
     h.registerHelper({ ...options.components });
-
     // TODO: register partials
-
     const entryFile = await fs.open(options.entry, 'r');
     const entryFileContent = await entryFile.readFile('utf-8');
-
     const template = h.compile(entryFileContent, handlebarsOptions);
     const generatedText = template(options.data);
-
     // TODO: Output file
     console.log(generatedText);
 }
